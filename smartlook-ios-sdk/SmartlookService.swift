b5886cd6a9e2653666bed0bd70f17437bcb76446
saiful <saifu>
2023-01-05 15:11:11 +0700

    add .travis.yml

---
+++ smartlook-ios-sdk/SmartlookService.swift
@@ -0,0 +1,14 @@
+//
+//  SmartlookService.swift
+//  smartlook-ios-sdk
+//
+//  Created by Saiful Islam on 05/01/23.
+//
+
+import SmartlookAnalytics
+
+public final class SmartlookService {
+    
+    public static let shared: Smartlook = Smartlook.instance
+    
+}
