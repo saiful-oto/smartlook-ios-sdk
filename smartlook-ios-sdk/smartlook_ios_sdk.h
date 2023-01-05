7a67d3bfbdca161752e7eb5295633f4e4c4df7b8
saiful <saifu>
2023-01-05 14:56:02 +0700

    initial commit

---
+++ smartlook-ios-sdk/smartlook_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  smartlook_ios_sdk.h
+//  smartlook-ios-sdk
+//
+//  Created by Saiful Islam on 05/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for smartlook_ios_sdk.
+FOUNDATION_EXPORT double smartlook_ios_sdkVersionNumber;
+
+//! Project version string for smartlook_ios_sdk.
+FOUNDATION_EXPORT const unsigned char smartlook_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <smartlook_ios_sdk/PublicHeader.h>
+
+
